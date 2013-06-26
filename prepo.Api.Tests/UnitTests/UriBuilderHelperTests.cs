6052ac8dae0bd325c95a7f081a0f4f0cfafcedf2
Sam Plews <the@codeite.com>
2013-06-26 18:44:23 +0100

    Added UriBuilderHelper

---
+++ prepo.Api.Tests/UnitTests/UriBuilderHelperTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using NUnit.Framework;
+using prepo.Api.Infrastructure;
+
+namespace prepo.Api.Tests.UnitTests
+{
+    [TestFixture]
+    public class UriBuilderHelperTests
+    {
+        [TestCase("left", "right", "left/right")]
+        [TestCase("leftA/leftB", "rightA/rightB", "leftA/leftB/rightA/rightB")]
+        [TestCase("/left/", "/right/", "/left/right/")]
+        [TestCase("/", "/right/", "/right/")]
+        [TestCase("/", "right/", "/right/")]
+        public void CombinesTwoPartsCorrectly(string left, string right, string expected)
+        {
+            // Arrange
+
+            // Act
+            var actual = UriBuilderHelper.Combine(left, right);
+
+            // Assert
+            actual.Should().Be(expected);
+        }
+    }
+}
---
+++ prepo.Api/Infrastructure/UriBuilderHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace prepo.Api.Infrastructure
+{
+    public static class UriBuilderHelper
+    {
+        public static string Combine(string first, params string[] parts)
+        {
+            if (parts.Length == 0)
+            {
+                return first;
+            }
+
+            first = first.TrimEnd('/');
+            var last = parts.Last().TrimStart('/');
+            var rest = parts.Take(parts.Length - 1).Select(p => p.Trim('/'));
+
+            var all = new[] {first}.Concat(rest).Concat(new[] {last});
+
+            return string.Join("/", all);
+        }
+    }
+}
